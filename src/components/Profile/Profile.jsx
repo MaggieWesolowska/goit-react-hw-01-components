8564d30a9409d46ccf418cccfbcba32d69afb027
MaggieWesolowska <maggiewes07@gmail.com>
2023-05-10 19:24:17 +0200

    edited components

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ export const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
     <div class="profile">
       <div class="description">
-        <img src={avatar} alt="User avatar" class="avatar" />
+        <img src={avatar} alt="User avatar" class="avatar" width="48" />
         <p class="name">{username}</p>
         <p class="tag">@{tag}</p>
         <p class="location">{location}</p>
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types';
 export const Statistics = ({ title, stats }) => {
   return (
     <section class="statistics">
-      <h2 class="title">{title}</h2>
+      {<h2 class="title">{title}</h2>}
 
       <ul>
         {stats.map(stat => (
           <li key={stat.id}>
-            <span className>{stat.label}</span>
-            <span className>{stat.percentage}</span>
+            <span>{stat.label}</span>
+            <span>{stat.percentage}</span>
           </li>
         ))}
       </ul>
