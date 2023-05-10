9285c08b427da3119aef8c0462763d9030a92294
MaggieWesolowska <maggiewes07@gmail.com>
2023-05-10 15:56:40 +0200

    added profile component, edited json

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,39 @@
+import propTypes from 'prop-types';
+
+export const Profile = ({ username, tag, location, avatar, stats }) => {
+  <div class="profile">
+    <div class="description">
+      <img src={avatar} alt="User avatar" class="avatar" />
+      <p class="name">{username}</p>
+      <p class="tag">@{tag}</p>
+      <p class="location">{location}</p>
+    </div>
+
+    <ul class="stats">
+      <li>
+        <span class="label">Followers</span>
+        <span class="quantity">{stats.follower}</span>
+      </li>
+      <li>
+        <span class="label">Views</span>
+        <span class="quantity">{stats.views}</span>
+      </li>
+      <li>
+        <span class="label">Likes</span>
+        <span class="quantity">{stats.likes}</span>
+      </li>
+    </ul>
+  </div>;
+};
+
+Profile.propTypes = {
+  username: propTypes.string.isRequired,
+  avatar: propTypes.string.isRequired,
+  tag: propTypes.string.isRequired,
+  location: propTypes.string.isRequired,
+  stats: propTypes.shape({
+    followers: propTypes.number,
+    views: propTypes.number,
+    likes: propTypes.number,
+  }),
+};
