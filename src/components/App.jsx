2d181d0c1eb88cf0bc7457d6ff911ca258cdb383
MaggieWesolowska <maggiewes07@gmail.com>
2023-05-10 19:02:39 +0200

    edited transactions file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,8 @@ import { Statistics } from './Statistics/Statistics';
 import data from 'components/Statistics/data.json';
 import { FriendList } from './FriendList/FriendList';
 import friends from 'components/FriendList/friends.json';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from 'components/TransactionHistory/transactions.json';
 
 export const App = () => {
   return (
@@ -27,6 +29,7 @@ export const App = () => {
       <Statistics title="Upload stats" stats={data} />
       {/* <Statistics stats={data} /> */}
       <FriendList friends={friends} />;
+      <TransactionHistory items={transactions} />;
     </div>
   );
 };
--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 
-export const TransactionHistory = ({ id, type, amount, currency }) => {
+export const TransactionHistory = ({ items }) => {
   return (
     <table class="transaction-history">
       <thead>
@@ -10,19 +10,24 @@ export const TransactionHistory = ({ id, type, amount, currency }) => {
           <th>Currency</th>
         </tr>
       </thead>
-
-      <tbody>
-        <tr>
-          <td>Invoice</td>
-          <td>125</td>
-          <td>USD</td>
-        </tr>
-        <tr>
-          <td>Withdrawal</td>
-          <td>85</td>
-          <td>USD</td>
-        </tr>
-      </tbody>
+      {items.map(item => (
+        <tbody>
+          <tr key={item.id}>
+            <td>{item.type}</td>
+            <td>{item.amount}</td>
+            <td>{item.currency}</td>
+          </tr>
+        </tbody>
+      ))}
     </table>
   );
+};
+
+TransactionHistory.propTypes = {
+  items: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    amount: PropTypes.number.isRequired,
+    currency: PropTypes.number.isRequired,
+  }),
 };
