332cf7d7577f2834679d91e4b64cd551e9a91bff
MaggieWesolowska <maggiewes07@gmail.com>
2023-05-12 18:35:11 +0200

    edited statistics component

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,6 +1,11 @@
 import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
 
+function getRandomHexColor() {
+  let bgColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return bgColor;
+}
+
 export const Statistics = ({ title, stats }) => {
   return (
     <section className={css.section}>
@@ -8,7 +13,11 @@ export const Statistics = ({ title, stats }) => {
 
       <ul className={css.stats}>
         {stats.map(stat => (
-          <li className={css.item} key={stat.id}>
+          <li
+            className={css.item}
+            style={{ backgroundColor: getRandomHexColor() }}
+            key={stat.id}
+          >
             <span className={css.label}>{stat.label}</span>
             <span className={css.percentage}>{stat.percentage}</span>
           </li>
@@ -17,6 +26,23 @@ export const Statistics = ({ title, stats }) => {
     </section>
   );
 };
+
+// const getBgColor = variant => {
+//   switch (variant) {
+//     case 'doc':
+//       return 'blue';
+//     case 'pdf':
+//       return 'green';
+//     case 'mp3':
+//       return 'red';
+//     case 'psd':
+//       return 'orange';
+//     case 'pdf2':
+//       return 'yellow';
+//     default:
+//       throw new Error(`Unsupported variant prop value - ${variant}`);
+//   }
+// };
 
 Statistics.propTypes = {
   title: PropTypes.string.isRequired,
