00f90f08380ae5d4cf4e8b69752f934a7c3749e4
MaggieWesolowska <maggiewes07@gmail.com>
2023-05-13 16:23:08 +0200

    removed unused code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,13 +11,10 @@ export const App = () => {
   return (
     <div
       style={{
-        // height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         flexDirection: 'column',
-        // fontSize: 40,
-        color: '#010101',
       }}
     >
       <Profile
